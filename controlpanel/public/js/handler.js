34d00218e1b4b8995de3c2631586e4d79de7c4a2
Sebastian Kiepsch <sebastian.kiepsch@telekom.de>
2015-11-19 10:02:44 +0000

    Initial Commit

---
+++ controlpanel/public/js/handler.js
@@ -0,0 +1,30 @@
+﻿startopenhab = function () {
+    $.post("/openhab", { action: "start" }, function (data) {
+        $('#console').append('\nstdout: ' + data.stdout);
+        $('#console').append('\nsterr: ' + data.stderr);
+        $('#console').append('error: ' + data.error);
+    });
+}
+stopopenhab = function () {
+    $.post("/openhab", { action: "stop" }, function (data) { 
+        $('#console').append('stdout: ' + data.stdout);
+        $('#console').append('sterr: ' + data.stderr);
+        $('#console').append('error: ' + data.error);
+    });
+}
+
+startvolume = function () {
+    $.post("/volume", { action: "start" }, function (data) { 
+        $('#console').append('stdout: ' + data.stdout);
+        $('#console').append('sterr: ' + data.stderr);
+        $('#console').append('error: ' + data.error);
+    });
+}
+
+stopvolume = function () {
+    $.post("/volume", { action: "stop" }, function (data) { 
+        $('#console').append('stdout: ' + data.stdout);
+        $('#console').append('sterr: ' + data.stderr);
+        $('#console').append('error: ' + data.error);
+    });
+}
---
+++ controlpanel/routes/index.js
@@ -0,0 +1,46 @@
+﻿var express = require('express');
+var exec = require('child_process').exec;
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.post('/openhab', function (req, res) {
+    if (req.body.action == "start") {
+        exec("sudo /etc/init.d/openhab start", function (error, stdout, stderr) { 
+            res.send({ stdout: stdout, stderr: stderr, error: error });
+        });
+    }
+    if (req.body.action == "stop") {
+        exec("sudo /etc/init.d/openhab stop", function (error, stdout, stderr) {
+            res.send({ stdout: stdout, stderr: stderr, error: error });
+        });
+    }
+    if (req.body.action == "status") {
+        exec("sudo /etc/init.d/openhab status", function (error, stdout, stderr) {
+            res.send({ stdout: stdout, stderr: stderr, error: error });
+        });
+    }
+});
+
+router.post('/volume', function (req, res) {
+    if (req.body.action == "start") {
+        exec("sudo /etc/init.d/volumesensor start", function (error, stdout, stderr) {
+            res.send({ stdout: stdout, stderr: stderr, error: error });
+        });
+    }
+    if (req.body.action == "stop") {
+        exec("sudo /etc/init.d/volumesensor stop", function (error, stdout, stderr) {
+            res.send({ stdout: stdout, stderr: stderr, error: error });
+        });
+    }
+    if (req.body.action == "status") {
+        exec("sudo /etc/init.d/volumesensor status", function (error, stdout, stderr) {
+            res.send({ stdout: stdout, stderr: stderr, error: error });
+        });
+    }
+});
+
+module.exports = router;
