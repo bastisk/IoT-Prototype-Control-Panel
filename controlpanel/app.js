118e716578797f782e7f36375bbf6a4f845703c4
Sebastian Kiepsch <basti.sk@gmx.de>
2015-12-08 09:45:48 +0100

    fixed handler.js

--- controlpanel/app.js
+++ controlpanel/app.js
@@ -24,7 +24,7 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/api', routes);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- controlpanel/public/js/handler.js
+++ controlpanel/public/js/handler.js
@@ -1,5 +1,34 @@
-﻿sendcommand= function (item, action){
-    $.post("/api/" + item, { action: action }, function (data) {
+﻿document.getElementById("startopenhab").onclick = startopenhab;
+document.getElementById("stopopenhab").onclick = stopopenhab;
+document.getElementById("startvolume").onclick = startvolume;
+document.getElementById("stopvolume").onclick = stopvolume;
+
+function startopenhab() {
+    $.post("/api/openhab", { action: "start" }, function (data) {
+        $('#console').append('\nstdout: ' + data.stdout);
+        $('#console').append('\nsterr: ' + data.stderr);
+        $('#console').append('\nerror:' + data.error);
+    });
+}
+
+function stopopenhab() {
+    $.post("/api/openhab", { action: "stop" }, function (data) {
+        $('#console').append('\nstdout: ' + data.stdout);
+        $('#console').append('\nsterr: ' + data.stderr);
+        $('#console').append('\nerror:' + data.error);
+    });
+}
+
+function startvolume() {
+    $.post("/api/volume", { action: "start" }, function (data) {
+        $('#console').append('\nstdout: ' + data.stdout);
+        $('#console').append('\nsterr: ' + data.stderr);
+        $('#console').append('\nerror:' + data.error);
+    });
+}
+
+function stopvolume() {
+    $.post("/api/volume", { action: "stop" }, function (data) {
         $('#console').append('\nstdout: ' + data.stdout);
         $('#console').append('\nsterr: ' + data.stderr);
         $('#console').append('\nerror:' + data.error);
